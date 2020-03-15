91fdf0cadb8a1ec56a32f276864b832e00e6b4f2
DimaN_Fateev <fateev.da@phystech.edu>
2020-03-15 21:14:23 +0300

    second

--- 02_convert.py
+++ 02_convert.py
@@ -8,7 +8,10 @@ from pathlib import Path
 from tqdm import tqdm
 
 def main():
-    raw_root = Path('corpus/raw/politics')
+    raw_root = Path('corpus/raw/')
+    
+    all_genre = ['Политика', 'В мире', 'Экономика', 'Общество', 'Происшествия',
+                 'Армия', 'Наука', 'Культура', 'Религия', 'Спорт', 'Туризм']
 
     # Включаем кэшировие загруженных страниц
     clean_root = Path('corpus/clean')
@@ -16,31 +19,39 @@ def main():
 
     # Пройдём по ранее скачанным страницам
     content = []
-#     qwer = 1
     for f in tqdm(raw_root.iterdir()):
-        name_file = str(f)[len('corpus/raw/politics/'):(len(str(f)) - len('.html'))]
-        
+        name_file = str(f)[len('corpus/raw/'):(len(str(f)) - len('.html'))]
         if f.suffix.lower() != '.html':
             continue
 
         with open(f, 'r', encoding='utf-8') as src:
             page_text = src.read()
-        
+            
         # Разберём страницу
         page_bs = bs4.BeautifulSoup(page_text)
         # Все нормальные страницы 
         page_content: bs4.element.Tag = page_bs.find_all(attrs={'class':'article__text'})
-            
-            
+        page_genre: bs4.element.Tag = page_bs.find_all(attrs={'class':'article__tags'})
+        
+        # Определяем жанр новости
+        for i in range(len(page_genre)):
+            for genre_i in all_genre:
+                if (genre_i in page_genre[i].get_text()):
+                    genre = genre_i
+                    break
+            if (genre_i in page_genre[i].get_text()):
+                genre = genre_i
+                break
 
-        if page_content is not None:
+        # Вытаскиваем текст       
+        if page_content is not None and genre is not None:
             one_content = []
-            for i in range(len(page_content) - 1):
+            for i in range(1, len(page_content) - 1):
                 one_content += list(map(str.strip, page_content[i].get_text().split('\n')))
-                d = ' '.join(one_content)
+                d = '\n'.join(one_content)
             content.append(d)
-            
-        with open(clean_root / name_file, 'w', encoding='utf-8') as dst:
+
+        with open(clean_root / genre /name_file, 'w', encoding='utf-8') as dst:
             c = d
             print(c, file=dst)
 
--- 04_statistics.py
+++ 04_statistics.py
@@ -12,12 +12,12 @@ def main() -> None:
     corpus_root = Path('corpus')
     # Читаем данные корпуса
     with open(corpus_root /'clean' / 'data.txt', 'r', encoding='utf-8') as src:
-        proverbs = [line.rstrip('\r\n') for line in src]
+        news = [line.rstrip('\r\n') for line in src]
     # Считаем статистику
     with open(corpus_root / 'statistics.txt', 'w', encoding='utf-8') as dst:
-        print('Всего новостей:', len(proverbs), file=dst)
+        print('Всего новостей:', len(news), file=dst)
 
-        words: List[str] = [w.lower().strip('.,') for p in proverbs for w in p.split()]
+        words: List[str] = [w.lower().strip('.,') for p in news for w in p.split()]
         print('Всего слов:', len(words), file=dst)
         unique_words = Counter(words)
         print('Уникальных слов (токенов):', len(unique_words), file=dst)
@@ -26,7 +26,7 @@ def main() -> None:
             print(f'\t{i}. {w} -> {freq}', file=dst)
         
         print('Количество символов (включая пробелы):',
-              sum(len(p) for p in proverbs), file=dst)
+              sum(len(p) for p in news), file=dst)
 
 
 if __name__ == "__main__":
